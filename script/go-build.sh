36e7ce0b89c74edeb20cd347acada4bcbd5e9bc4
ParkNams <nspark@toptoon.com>
2022-12-21 12:33:19 +0900

    build script

--- script/go-build.sh
+++ script/go-build.sh
@@ -6,11 +6,11 @@ pwd
 
 go build logFile.com/log-file-go
 
-[ ! -d "./go-build" ] && mkdir $HOME/go-build
-[ ! -d "./go-build/log-file-go" ] && mkdir $HOME/go-build/log-file-go
-[ ! -f "$HOME/go-build/log-file-go/log-file-go" ] && rm -rf $HOME/go-build/log-file-go/log-file-go 
+[ ! -d "./go-build" ] && mkdir ./go-build
+[ ! -d "./go-build/log-file-go" ] && mkdir ./go-build/log-file-go
+[ ! -f "./go-build/log-file-go/log-file-go" ] && rm -rf ./go-build/log-file-go/log-file-go 
 
-mv ./log-file-go $HOME/go-build/log-file-go
+mv ./log-file-go ./go-build/log-file-go
 
-chmod 777 $HOME/go-build/log-file-go/log-file-go
+chmod 777 ./go-build/log-file-go/log-file-go
 
