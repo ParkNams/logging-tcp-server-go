5467c130f5888a1892fda29896d81f523df0cd68
ParkNams <nspark@toptoon.com>
2023-01-02 16:54:01 +0900

    scrpit ignore remove

--- script/open-cpu-prof.sh
+++ script/open-cpu-prof.sh
@@ -4,8 +4,8 @@ cd "/logging-batch-go/logs/cpu"
 
 pwd
 
-echo ":6061 openFile.prof" 
+echo ":6061 openHttp.prof" 
 
 fuser -k -n tcp 6061
 
-go tool pprof -http :6061 openFile.prof
+go tool pprof -http :6061 openHttp.prof
--- script/open-mem-prof.sh
+++ script/open-mem-prof.sh
@@ -4,8 +4,8 @@ cd "/logging-batch-go/logs/mem"
 
 pwd
 
-echo ":6062" "openFile.prof" 
+echo ":6062" "openHttp.prof" 
 
 fuser -k -n tcp 6062
 
-go tool pprof -http :6062 openFile.prof
+go tool pprof -http :6062 openHttp.prof
