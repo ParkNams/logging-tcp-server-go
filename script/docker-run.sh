15265808ae270bc4f784ffd39ea76e02042c571a
ParkNams <nspark@toptoon.com>
2023-02-27 08:15:13 +0900

    test

--- script/docker-run.sh
+++ script/docker-run.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+rm -rf .env
+
+touch .env
+
+cat <<EOF >> .env
+ TCP_PORT=":8000"
+ ENVIRONMENT="container"
+ LOGGING_BUCKET="topco-log-profilig-bucket"
+ AWS_CREDENTIAL_PROFILE="playground"
+EOF 
+
 docker stop logging-batch-go-container
 docker rmi logging-batch-go-image
 docker build -t logging-batch-go-image .
+
+
+
 
 echo "$HOME" | while read path ;
 do
